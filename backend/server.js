dbeecbeb306d639dd74fe82973d82bb654d080ac
patiuaarondhvsu <2021305281@dhvsu.edu.ph>
2025-09-26 10:50:21 +0800

    auth token working

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
 
 // express app
 const app = express();
@@ -18,7 +19,8 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.use('/api/workouts',workoutRoutes)
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to DB
 mongoose.connect(process.env.MONGO_URI)
